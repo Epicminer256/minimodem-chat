06b0d187bf659b84ca0b8dd921e3015c3458a380
IanKing <72170170+Epicminer256@users.noreply.github.com>
2020-11-20 15:58:08 -0600

    Add files via upload

---
+++ minimodem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo Revice = rx
+echo Transmit = tx
+read direction
+
+echo pick channel from 1 to 8
+read chchannel
+let channel1=$chchannel*1000
+let channel2=$chchannel*1000+300
+
+echo Rate of speed
+echo recommended is 140
+read ratespeed
+clear
+echo ---------------------
+echo      $direction      
+echo ---------------------
+minimodem --quiet --mark $channel1 --space $channel2 --stopbits 2.5 --ascii --$direction $ratespeed
